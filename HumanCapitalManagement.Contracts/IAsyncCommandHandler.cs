b144cfa72139afa0ca1ba0f0145f3d26aaafc6d0
ToniTrifonov <toni.trifonov@abv.bg>
2025-04-03 10:09:59 +0300

    Create query and command handlers interfaces and add commands, queries and results folders to new project

---
+++ HumanCapitalManagement.Contracts/IAsyncCommandHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace HumanCapitalManagement.Contracts
+{
+    public interface IAsyncCommandHandler<TCommand, TResult>
+        where TCommand : class
+        where TResult : class
+    {
+        Task<TResult> HandleAsync(TCommand command);
+    }
+}
---
+++ HumanCapitalManagement.Contracts/IAsyncQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace HumanCapitalManagement.Contracts
+{
+    public interface IAsyncQueryHandler<TQuery, TResult>
+        where TQuery : class
+        where TResult : class
+    {
+        Task<TResult> HandleAsync(TQuery query);
+    }
+}
