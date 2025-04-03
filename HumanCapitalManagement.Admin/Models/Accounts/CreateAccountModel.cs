a560205fb59998d7b1f5342ef3bf43262fb701d5
ToniTrifonov <toni.trifonov@abv.bg>
2025-04-03 18:11:59 +0300

    Add account creation form

--- HumanCapitalManagement.Admin/Models/Accounts/CreateAccountModel.cs
+++ HumanCapitalManagement.Admin/Models/Accounts/CreateAccountModel.cs
@@ -2,5 +2,12 @@
 {
     public class CreateAccountModel
     {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public string RoleId { get; set; }
     }
 }
