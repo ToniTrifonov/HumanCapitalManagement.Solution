5259eb98a4376080621ce67632df8135633f6240
ToniTrifonov <toni.trifonov@abv.bg>
2025-04-06 10:19:15 +0300

    Remove useless action methods and views, and reorganize buttons

--- HumanCapitalManagement.Admin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HumanCapitalManagement.Admin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
