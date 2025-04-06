052b8eee9c295297ab877ac2818e007a7362cfb8
ToniTrifonov <toni.trifonov@abv.bg>
2025-04-06 09:47:35 +0300

    Refactor ViewDataHelper class to use static constructor for initializaton fo static fields

--- HumanCapitalManagement.Admin/ViewHelpers/ViewDataHelper.cs
+++ HumanCapitalManagement.Admin/ViewHelpers/ViewDataHelper.cs
@@ -5,35 +5,26 @@ namespace HumanCapitalManagement.Web.ViewHelpers
 {
     public static class ViewDataHelper
     {
-        private static List<SelectListItem> allRoles;
-        private static List<SelectListItem> employeePositions;
+        private readonly static List<SelectListItem> allRoles;
+        private readonly static List<SelectListItem> employeePositions;
 
-        public static List<SelectListItem> AllRoles()
+        static ViewDataHelper()
         {
-            if (allRoles == null)
+            allRoles = Enum.GetValues<UserRole>().Select(v => new SelectListItem
             {
-                allRoles = Enum.GetValues(typeof(UserRole)).Cast<UserRole>().Select(v => new SelectListItem
-                {
-                    Text = v.ToString(),
-                    Value = v.ToString()
-                }).ToList();
-            }
+                Text = v.ToString(),
+                Value = v.ToString()
+            }).ToList();
 
-            return allRoles;
+            employeePositions = Enum.GetValues<EmployeePosition>().Select(v => new SelectListItem
+            {
+                Text = v.ToString(),
+                Value = ((int)v).ToString()
+            }).ToList();
         }
 
-        public static List<SelectListItem> AllEmployeePositions()
-        {
-            if (employeePositions == null)
-            {
-                employeePositions = Enum.GetValues(typeof(EmployeePosition)).Cast<EmployeePosition>().Select(v => new SelectListItem
-                {
-                    Text = v.ToString(),
-                    Value = ((int)v).ToString()
-                }).ToList();
-            }
+        public static List<SelectListItem> AllRoles => allRoles;
 
-            return employeePositions;
-        }
+        public static List<SelectListItem> AllEmployeePositions => employeePositions;
     }
 }
