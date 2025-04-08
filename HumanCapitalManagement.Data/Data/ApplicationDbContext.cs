22e1bd72d8b9a06b2f7cdd7873ef17282aaef8cf
ToniTrifonov <toni.trifonov@abv.bg>
2025-04-08 21:01:18 +0300

    Add accounts repository and projects repository tests

--- HumanCapitalManagement.Data/Data/ApplicationDbContext.cs
+++ HumanCapitalManagement.Data/Data/ApplicationDbContext.cs
@@ -6,6 +6,10 @@ namespace HumanCapitalManagement.Data.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext()
+        {    
+        }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ HumanCapitalManagement.Tests/Repositories/AccountsRepositoryTests.cs
@@ -0,0 +1,56 @@
+﻿using HumanCapitalManagement.Data.Data;
+using HumanCapitalManagement.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace HumanCapitalManagement.Tests.Repositories
+{
+    [TestClass]
+    public class AccountsRepositoryTests
+    {
+        private readonly AccountsRepository repository;
+        private readonly Mock<ApplicationDbContext> contextMock;
+        private readonly Mock<DbSet<IdentityUser>> usersMock;
+
+        public AccountsRepositoryTests()
+        {
+            contextMock = new Mock<ApplicationDbContext>();
+            usersMock = new Mock<DbSet<IdentityUser>>();
+            contextMock.Setup(x => x.Set<IdentityUser>()).Returns(usersMock.Object);
+
+            repository = new AccountsRepository(contextMock.Object);
+        }
+
+        [TestMethod]
+        public async Task Add_ShouldCorrectlyAddEntityToCollection()
+        {
+            // Arrange
+            var newUser = new IdentityUser()
+            {
+                Id = "123",
+                UserName = "testUsername",
+                Email = "test@test.com"
+            };
+
+            // Act
+            await repository.Add(newUser);
+
+            // Assert
+            usersMock.Verify(x => x.AddAsync(newUser, default), Times.Once(), "AddAsync should be called once.");
+            contextMock.Verify(x => x.SaveChangesAsync(default), Times.Once(), "SaveChangesAsync should be called once.");
+        }
+
+        [TestMethod]
+        public async Task UserEmailInUse_ShouldVerifyEmailAvailability()
+        {
+            // Arrange
+
+            // Act
+            var result = await repository.UserEmailInUse("test");
+
+            // Assert
+            usersMock.Verify(x => x.AnyAsync(default), Times.Once(), "AnyAsync should be called once.");
+        }
+    }
+}
