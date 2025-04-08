b878f63c4f2efacae9b33ae62228ebcccbeac21c
ToniTrifonov <toni.trifonov@abv.bg>
2025-04-08 15:57:58 +0300

    Add test for employee deletion command handler

---
+++ HumanCapitalManagement.Tests/Commands/Employees/Delete/DeleteEmployeeCommandHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using HumanCapitalManagement.Contracts.Commands.Employees;
+using HumanCapitalManagement.Data.Contracts;
+using HumanCapitalManagement.Data.Entities;
+using HumanCapitalManagement.Handlers.Commands.Employees.Delete;
+using Moq;
+
+namespace HumanCapitalManagement.Tests.Commands.Employees.Delete
+{
+    [TestClass]
+    public class DeleteEmployeeCommandHandlerTests
+    {
+        private readonly DeleteEmployeeCommandHandler handler;
+
+        private readonly Mock<IEmployeesRepository> employeesRepositoryMock;
+
+        public DeleteEmployeeCommandHandlerTests()
+        {
+            employeesRepositoryMock = new Mock<IEmployeesRepository>();
+
+            handler = new DeleteEmployeeCommandHandler(employeesRepositoryMock.Object);
+        }
+
+        [TestMethod]
+        public async Task HandleAsync_ShouldReturnFailedResultWithCorrectErrorMessage_WhenEmployeeDoesNotExist()
+        {
+            // Arrange
+            employeesRepositoryMock
+                .Setup(x => x.EmployeeById(It.IsAny<string>()))
+                .ReturnsAsync((Employee?)null);
+
+            var command = new DeleteEmployeeCommand("test");
+
+            // Act
+            var result = await this.handler.HandleAsync(command);
+
+            // Assert
+            Assert.AreEqual("An employee with that id does not exist.", result.ErrorMessage);
+            Assert.AreEqual(false, result.Succeed);
+        }
+
+        [TestMethod]
+        public async Task HandleAsync_ShouldReturnSuccessfulResult_WhenEmployeeExists()
+        {
+            // Arrange
+            employeesRepositoryMock
+                .Setup(x => x.EmployeeById(It.IsAny<string>()))
+                .ReturnsAsync(new Employee());
+
+            var command = new DeleteEmployeeCommand("test");
+
+            // Act
+            var result = await this.handler.HandleAsync(command);
+
+            // Assert
+            Assert.IsNull(result.ErrorMessage);
+            Assert.AreEqual(true, result.Succeed);
+        }
+    }
+}
---
+++ HumanCapitalManagement.Tests/Commands/Employees/Delete/DeleteEmployeeErrorHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using HumanCapitalManagement.Contracts;
+using HumanCapitalManagement.Contracts.Commands.Employees;
+using HumanCapitalManagement.Contracts.Results.Employees;
+using HumanCapitalManagement.Handlers.Commands.Employees.Delete;
+using Moq;
+
+namespace HumanCapitalManagement.Tests.Commands.Employees.Delete
+{
+    [TestClass]
+    public class DeleteEmployeeErrorHandlerTests
+    {
+        private readonly DeleteEmployeeErrorHandler errorHandler;
+
+        private readonly Mock<IAsyncCommandHandler<DeleteEmployeeCommand, DeleteEmployeeResult>> deleteEmployeeMock;
+
+        public DeleteEmployeeErrorHandlerTests()
+        {
+            deleteEmployeeMock = new Mock<IAsyncCommandHandler<DeleteEmployeeCommand, DeleteEmployeeResult>>();
+
+            errorHandler = new DeleteEmployeeErrorHandler(deleteEmployeeMock.Object);
+        }
+
+        [TestMethod]
+        public async Task HandleAsync_ShouldReturnFailedResultWithCorrectErrorMessage_WhenExceptionIsThrown()
+        {
+            // Arrange
+            deleteEmployeeMock
+                .Setup(x => x.HandleAsync(It.IsAny<DeleteEmployeeCommand>()))
+                .ThrowsAsync(new Exception());
+
+            var command = new DeleteEmployeeCommand("test");
+
+            // Act
+            var result = await this.errorHandler.HandleAsync(command);
+
+            // Assert
+            Assert.AreEqual(false, result.Succeed);
+            Assert.AreEqual("An unexpected error occurred.", result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public async Task HandleAsync_ShouldReturnTheResultOfTheHandlerItDecorates_WhenNoException()
+        {
+            // Arrange
+            var expectedResult = new DeleteEmployeeResult();
+            deleteEmployeeMock
+                .Setup(x => x.HandleAsync(It.IsAny<DeleteEmployeeCommand>()))
+                .ReturnsAsync(expectedResult);
+
+            var command = new DeleteEmployeeCommand("test");
+
+            // Act
+            var result = await this.errorHandler.HandleAsync(command);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
