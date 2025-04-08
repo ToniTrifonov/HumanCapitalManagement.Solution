5ff78b78d317e108c6d9ce6835fee752da690672
ToniTrifonov <toni.trifonov@abv.bg>
2025-04-08 16:02:57 +0300

    Add tests for edit employee command handlers

---
+++ HumanCapitalManagement.Tests/Commands/Employees/Edit/EditEmployeeCommandHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿using HumanCapitalManagement.Contracts.Commands.Employees;
+using HumanCapitalManagement.Data.Contracts;
+using HumanCapitalManagement.Data.Entities;
+using HumanCapitalManagement.Data.Enums;
+using HumanCapitalManagement.Handlers.Commands.Employees.Edit;
+using Moq;
+
+namespace HumanCapitalManagement.Tests.Commands.Employees.Edit
+{
+    [TestClass]
+    public class EditEmployeeCommandHandlerTests
+    {
+        private readonly EditEmployeeCommandHandler handler;
+
+        private readonly Mock<IEmployeesRepository> employeesRepositoryMock;
+
+        public EditEmployeeCommandHandlerTests()
+        {
+            employeesRepositoryMock = new Mock<IEmployeesRepository>();
+
+            handler = new EditEmployeeCommandHandler(employeesRepositoryMock.Object);
+        }
+
+        [TestMethod]
+        public async Task HandleAsync_ShouldReturnFailedResultWithCorrectErrorMessage_WhenEmployeeDoesNotExist()
+        {
+            // Arrange
+            employeesRepositoryMock
+                .Setup(x => x.EmployeeById(It.IsAny<string>()))
+                .ReturnsAsync((Employee?)null);
+
+            var command = new EditEmployeeCommand("test", "test", "test", EmployeePosition.BackEndDeveloper, 123);
+
+            // Act
+            var result = await this.handler.HandleAsync(command);
+
+            // Assert
+            Assert.AreEqual("Employee does not exist.", result.ErrorMessage);
+            Assert.AreEqual(false, result.Succeed);
+        }
+
+        [TestMethod]
+        public async Task HandleAsync_ShouldReturnSuccessfulResult_WhenEmployeeExists()
+        {
+            // Arrange
+            employeesRepositoryMock
+                .Setup(x => x.EmployeeById(It.IsAny<string>()))
+                .ReturnsAsync(new Employee());
+
+            var command = new EditEmployeeCommand("test", "test", "test", EmployeePosition.BackEndDeveloper, 123);
+
+            // Act
+            var result = await this.handler.HandleAsync(command);
+
+            // Assert
+            Assert.IsNull(result.ErrorMessage);
+            Assert.AreEqual(true, result.Succeed);
+        }
+    }
+}
---
+++ HumanCapitalManagement.Tests/Commands/Employees/Edit/EditEmployeeErrorHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using HumanCapitalManagement.Contracts;
+using HumanCapitalManagement.Contracts.Commands.Employees;
+using HumanCapitalManagement.Contracts.Results.Employees;
+using HumanCapitalManagement.Data.Enums;
+using HumanCapitalManagement.Handlers.Commands.Employees.Edit;
+using Moq;
+
+namespace HumanCapitalManagement.Tests.Commands.Employees.Edit
+{
+    public class EditEmployeeErrorHandlerTests
+    {
+        private readonly EditEmployeeErrorHandler errorHandler;
+
+        private readonly Mock<IAsyncCommandHandler<EditEmployeeCommand, EditEmployeeResult>> editEmployeeMock;
+
+        public EditEmployeeErrorHandlerTests()
+        {
+            editEmployeeMock = new Mock<IAsyncCommandHandler<EditEmployeeCommand, EditEmployeeResult>>();
+
+            errorHandler = new EditEmployeeErrorHandler(editEmployeeMock.Object);
+        }
+
+        [TestMethod]
+        public async Task HandleAsync_ShouldReturnFailedResultWithCorrectErrorMessage_WhenExceptionIsThrown()
+        {
+            // Arrange
+            editEmployeeMock
+                .Setup(x => x.HandleAsync(It.IsAny<EditEmployeeCommand>()))
+                .ThrowsAsync(new Exception());
+
+            var command = new EditEmployeeCommand("test", "test", "test", EmployeePosition.BackEndDeveloper, 123);
+
+            // Act
+            var result = await this.errorHandler.HandleAsync(command);
+
+            // Assert
+            Assert.AreEqual(false, result.Succeed);
+            Assert.AreEqual("An unexpected error occurred.", result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public async Task HandleAsync_ShouldReturnTheResultOfTheHandlerItDecorates_WhenNoException()
+        {
+            // Arrange
+            var expectedResult = new EditEmployeeResult();
+            editEmployeeMock
+                .Setup(x => x.HandleAsync(It.IsAny<EditEmployeeCommand>()))
+                .ReturnsAsync(expectedResult);
+
+            var command = new EditEmployeeCommand("test", "test", "test", EmployeePosition.BackEndDeveloper, 123);
+
+            // Act
+            var result = await this.errorHandler.HandleAsync(command);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
